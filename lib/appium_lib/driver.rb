5287a654e36333a22e3114ca58bf40e293a4276f
Justin <justin.ison@gmail.com>
2016-09-16 17:12:20 -0400

    remove selenium/client/errors

--- lib/appium_lib/driver.rb
+++ lib/appium_lib/driver.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'ap'
 gem 'selenium-webdriver', '~> 3.0.0.beta3.1'
 require 'selenium-webdriver'
-require 'selenium/client/errors' # used in helper.rb for CommandError
+#require 'selenium/client/errors' # used in helper.rb for CommandError
 require 'nokogiri'
 
 # common
